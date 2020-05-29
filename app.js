538f112e16bf23050469aaa946d9de79665cfebe
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-05-29 21:57:08 +0100

    computed properties, watchers, filters, more on loopings

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-new Vue({
+let vm1 = new Vue({
   el: "#root",
   data: {
     event: {
@@ -12,6 +12,8 @@ new Vue({
     nameText: "",
     guestName: [],
     formSubmitClass: "",
+    eventCapacity: 25,
+    eventCapacityPercentage: 0,
     appStyles: {
       marginTop: "2px",
       background: "steelblue",
@@ -21,11 +23,48 @@ new Vue({
   },
   methods: {
     handleSubmit: function () {
-      if ((this.nameText.length > 0) && (this.nameText.trim() !== ""))  {
+      if (
+        this.nameText.length > 0 &&
+        this.nameText.trim() !== "" &&
+        this.eventCapacityPercentage < 100
+      ) {
         this.guestName.push(this.nameText);
         this.nameText = "";
         this.formSubmitClass = "submitted";
+        this.eventCapacityPercentage =
+          (this.guestName.length * 100) / this.eventCapacity;
       }
+    },
+  },
+  computed: {
+    sortName: function () {
+      return this.guestName.sort();
+    },
+  },
+  filters: {
+    toUpper: function (val) {
+      return val.toUpperCase();
+    },
+    formatName: function (val) {
+      return val.slice(0, 1).toUpperCase() + val.slice(1).toLowerCase();
+    },
+  },
+});
+
+let vm2 = new Vue({
+  el: "#navigation",
+  data: {
+    appTitle: "A Guest List App By Vue.js",
+    navLinks: [
+      { name: "Home", id: 1, url: "https://www.google.com" },
+      { name: "Upcoming Events", id: 2, url: "https://www.amazon.com" },
+      { name: "Guests Benefits", id: 3, url: "https://www.ebay.com" },
+      { name: "Latest News", id: 4, url: "https://www.facebook.com" },
+    ],
+  },
+  methods: {
+    capacityChanged: function () {
+      vm1.eventCapacity = 15;
     },
   },
 });
